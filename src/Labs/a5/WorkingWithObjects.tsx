f7821d463a56388f4a41711d17b2e1944566cd22
mbaraty123 <mbaraty123@gmail.com>
2024-04-02 11:17:21 -0400

    lab 5 complete

--- src/Labs/a5/WorkingWithObjects.tsx
+++ src/Labs/a5/WorkingWithObjects.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 function WorkingWithObjects() {
 	const [assignment, setAssignment] = useState({
 		id: 1,
@@ -9,6 +11,19 @@ function WorkingWithObjects() {
 		score: 0,
 	});
 	const ASSIGNMENT_URL = "http://localhost:4000/a5/assignment";
+	const fetchAssignment = async () => {
+		const response = await axios.get(`${ASSIGNMENT_URL}`);
+		setAssignment(response.data);
+	};
+	const updateTitle = async () => {
+		const response = await axios.get(
+			`${ASSIGNMENT_URL}/title/${assignment.title}`
+		);
+		setAssignment(response.data);
+	};
+	useEffect(() => {
+		fetchAssignment();
+	}, []);
 
 	const [module, setModule] = useState({
 		id: "_1",
@@ -21,6 +36,19 @@ function WorkingWithObjects() {
 	return (
 		<div>
 			<h3>Working With Objects</h3>
+			<h3>Modifying Properties</h3>
+			<input
+				onChange={(e) =>
+					setAssignment({
+						...assignment,
+						title: e.target.value,
+					})
+				}
+				value={assignment.title}
+				type="text"
+			/>
+			<button onClick={updateTitle}>Update Title to: {assignment.title}</button>
+			<button onClick={fetchAssignment}>Fetch Assignment</button>
 			<h4>Retrieving Objects</h4>
 			<a className="btn btn-primary" href="http://localhost:4000/a5/assignment">
 				Get Assignment
